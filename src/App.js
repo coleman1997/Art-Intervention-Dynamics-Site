45549f40dcf43a5830fa6bf0cbfa7f1927d52373
colemankus <hanshi2929@gmail.com>
2024-02-14 15:19:07 -0600

    about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/components/AboutMainSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./AboutMainSection.css";
+
+const AboutMainSection = () => {
+  return (
+    <div className="about-page">
+      <div className="about-container">
+        <div className="about-left"></div>
+        <div className="about-right">
+          <div className="about-content">
+            <h1>Why Us?</h1>
+            <br />
+            <p className="about-p">
+              I am Melissa Kuseliauskas, the founder of Art Intervention
+              Dynamics, LLC. I have dedicated years developing this program. I
+              have conducted research and implemented this curriculum for two
+              and a half years with outstanding results and am currently
+              conducting more research and publishing an article on my findings.
+              I also have experience in the education system as a teacher and
+              researcher. I am very passionate about this program. I love seeing
+              individuals with special needs finally being able to communicate
+              and be their own person after years of frustration. I have seen
+              first hand people become much happier individuals. I will put
+              everything I have into tailoring my curriculums to every schools
+              needs and make sure that they have every tool needed to succeed.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMainSection;
