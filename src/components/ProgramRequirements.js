709cb7b2edfa7f584df836667f5f6e497d9625f0
colemankus <hanshi2929@gmail.com>
2024-02-20 11:05:27 -0600

    added services page content

--- src/components/ProgramRequirements.js
+++ src/components/ProgramRequirements.js
@@ -7,40 +7,40 @@ const ProgramRequirements = () => {
     <div className="program-page">
       <div className="program-container">
         <div className="program-content">
-          <h1>We are unlocking their desire to learn</h1>
+          <h1>Program Requirements</h1>
           <br />
           <h3>
-            {" "}
-            There is a great need for more effective learning strategies in the
-            Special Education System. With a teacher shortage, overwhelmed
-            teachers, and students with maladaptive behaviors due to a lack of
-            not being able to communicate... we, as a society, need help and
-            fast.
+            For this program to be effective, the requirements below are needed
           </h3>
           <br />
-          <p>
-            In all Special Education schools or organizations, there are
-            typically only one on one instruction learning strategies and they
-            are inadequate when it comes to improving language, focus and
-            behavior. They only offer extrinsic reward strategies (or token
-            systems) which have to always be provided in order to get a child or
-            young adult to do what is asked of them or to improve their
-            learning. The individual with special needs has no inner motivation
-            to improve or grow in learning...thereby resulting in poor
-            performance or progression.
-          </p>
+          <ul className="require-list">
+            <li>Our program works with ages 7 to adulthood</li>
+            <li>A self-contained room with a group of peers is needed</li>
+            <li>
+              A.I.D. is implemented for one hour twice a week during regular
+              school weeks
+            </li>
+            <li>Consistency in location of sessions is needed</li>
+          </ul>
           <br />
-          <p>
-            Art Intervention Dynamics offers peer group learning strategies that
-            improve language, focus, and behavior in a short amount of time. Our
-            strategy is unique because it offers a method that helps students
-            learn because they want to learn. It produces intrinsic rewards and
-            unlocks their desire to communicate. They want to learn and grow
-            because for the first time, they are being heard by peers and adults
-            without being told what to do. Nearly every student I've taught this
-            to becomes "social butterflies". It's like watching them blossom and
-            communicate for the first time.
-          </p>
+          <h1>What to expect</h1>
+          <br />
+          <h3>Art Intervention Dynamics offers:</h3>
+          <br />
+          <ul className="require-list">
+            <li>
+              A presentation of our original and research supported curriculum
+            </li>
+            <li>All materials needed to start the program</li>
+            <li>Expertise in the field</li>
+            <li>Staff training</li>
+            <li>Ongoing support and training as needed</li>
+          </ul>
+          <br />
+          <h3>
+            An introductory one-day service is needed. This can be 4-8 hours of
+            service depending on client’s availability.
+          </h3>
         </div>
       </div>
     </div>
@@ -48,4 +48,3 @@ const ProgramRequirements = () => {
 };
 
 export default ProgramRequirements;
-
